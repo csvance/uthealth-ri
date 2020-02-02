0fd57e32d25631388868fad670c2c6a2614b41ba
Carroll Vance <csvance@uh.edu>
2020-02-02 03:40:06 -0600

    yeee boi

--- run.py
+++ run.py
@@ -4,32 +4,54 @@ from keras.layers import *
 from keras.models import Model
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.regularizers import l2
+import numpy as np
+import pandas as pd
 import tensorflow as tf
+import os
+import pickle
+import plac
 
 from rectified_adam import RectifiedAdam
-from utils import fix_tf
 
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import roc_auc_score
 
 from cfg import *
 
-OPTIMIZER = RectifiedAdam(lr=LR)
+OPTIMIZER = RectifiedAdam(lr=LR, decay=LR / EPOCHS)
+
+
+def fix_tf():
+    # Fix Tensorflow Memory Usage
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
 
 def build_keras():
-
-    ip = Input((SEQ_LEN, 44))
+    ip = Input((INPUT_DIMS,))
 
     def f_lambda_smiles(x):
-        x = x[:, 0:SMILES_LEN, SMILES_IDX:42]
+        x = x[:, SMILES_IDX_I:SMILES_IDX_F]
+        x = tf.reshape(x, (-1, SMILES_LEN, SMILES_DIMS))
         return x
 
     def f_lambda_lines(x):
-        return x[:, 0:LINES_LEN, LINES_IDX]
+        return x[:, LINES_IDX_I:LINES_IDX_F]
 
     def f_lambda_maccs(x):
-        return x[:, 0:MACCS_LEN, MACCS_IDX]
+        return x[:, MACCS_IDX_I:MACCS_IDX_F]
+
+    def f_lambda_target(x):
+        return x[:, TARGET_IDX_I:TARGET_IDX_F]
 
     lambda_smiles = functools.partial(f_lambda_smiles)
     lambda_smiles.__name__ = 'lambda_smiles'
@@ -37,32 +59,40 @@ def build_keras():
     lambda_lines.__name__ = 'lambda_lines'
     lambda_maccs = functools.partial(f_lambda_maccs)
     lambda_maccs.__name__ = 'lambda_maccs'
+    lambda_target = functools.partial(f_lambda_target)
+    lambda_target.__name__ = 'lambda_target'
 
     features = []
 
     # Cell Lines
     l_dropout = Dropout(LINES_DROPOUT)
-    l_dense1 = Dense(DIMS, activation='tanh', kernel_regularizer=l2(L2))
 
     # forward
     lx = Lambda(lambda x: lambda_lines(x))(ip)
     if LINES_DROPOUT > 0:
         lx = l_dropout(lx)
-    lx = l_dense1(lx)
     features.append(lx)
 
     if SMILES_ENABLE:
-        s_conv1 = Conv1D(DIMS, 3, padding='same', activation='relu')
-        s_ap1 = AveragePooling1D()
-        s_conv2 = Conv1D(DIMS, 3, padding='same', activation='relu')
-        s_ap2 = AveragePooling1D()
+        s_conv1 = Conv1D(SMILES_KERNEL_DIMS, 11, padding='same', kernel_regularizer=l2(L2))
+        s_bn1 = BatchNormalization()
+        s_relu1 = ReLU()
+        s_ap1 = AveragePooling1D(pool_size=5)
+        s_conv2 = Conv1D(SMILES_KERNEL_DIMS, 11, padding='same', kernel_regularizer=l2(L2))
+        s_bn2 = BatchNormalization()
+        s_relu2 = ReLU()
+        s_ap2 = AveragePooling1D(pool_size=5)
         s_gmp = GlobalMaxPooling1D()
 
         # forward
         sx = Lambda(lambda x: lambda_smiles(x))(ip)
         sx = s_conv1(sx)
+        sx = s_bn1(sx)
+        sx = s_relu1(sx)
         sx = s_ap1(sx)
         sx = s_conv2(sx)
+        sx = s_bn2(sx)
+        sx = s_relu2(sx)
         sx = s_ap2(sx)
         sx = s_gmp(sx)
 
@@ -71,21 +101,31 @@ def build_keras():
     # MACCS
     if MACCS_ENABLE:
         m_dropout = Dropout(LINES_DROPOUT)
-        m_dense1 = Dense(DIMS, activation='tanh', kernel_regularizer=l2(L2))
 
         # forward
         mx = Lambda(lambda x: lambda_maccs(x))(ip)
         if MACCS_DROPOUT > 0:
             mx = m_dropout(mx)
-        mx = m_dense1(mx)
         features.append(mx)
 
+    if TARGET_ENABLE:
+        t_dropout = Dropout(TARGET_DROPOUT)
+
+        # forward
+        tx = Lambda(lambda x: lambda_target(x))(ip)
+        if MACCS_DROPOUT > 0:
+            tx = t_dropout(tx)
+        features.append(tx)
+
     j_concat = Concatenate()
-    cls = Dense(1, activation='sigmoid')
+    j_hidden = Dense(HIDDEN_DIMS, activation='tanh')
+    j_cls = Dense(1, activation='sigmoid')
 
     jx = j_concat(features)
+    if HIDDEN_DIMS > 0:
+        jx = j_hidden(jx)
 
-    y = cls(jx)
+    y = j_cls(jx)
 
     model = Model(inputs=ip, outputs=y)
     model.compile(optimizer=OPTIMIZER,
@@ -112,59 +152,129 @@ def build_adaboost():
     return model
 
 
-def main(cmd: str = 'train', dummy_data: bool = True):
+def idgc20(y_true, y_pred, cname, dname, submission: bool = False):
+    results = {
+        'uid': [],
+        'y_true': [],
+        'y_pred': [],
+        'cell': [],
+        'drug': []
+    }
+    if submission:
+        del results['y_true']
+
+    for i in range(0, y_pred.shape[0]):
+        results['uid'].append(i)
+        if not submission:
+            results['y_true'].append(y_true[i, 0])
+        results['y_pred'].append(y_pred[i, 1])
+        results['cell'].append(cname[i])
+        results['drug'].append(dname[i])
+
+    df = pd.DataFrame.from_dict(results)
+    if submission:
+        df['rank'] = 100
+
+    cells_drugs = []
+
+    all_idgc_20 = []
+    for cell in df['cell'].unique():
+
+        dfc = df[df['cell'] == cell]
+        dfc.sort_values(by=['y_pred'], ascending=False, inplace=True)
+
+        dgc_20 = 0
+        for i in range(0, min(20, dfc.shape[0])):
+            if submission:
+                df.loc[df['uid'] == dfc.iloc[i]['uid'], 'rank'] = i
+            if not submission:
+                dgc_20 += dfc.iloc[i]['y_true'] / np.log2(i + 2)
+        idgc_20 = 0
+        for i in range(0, min(20, dfc.shape[0])):
+            idgc_20 += 1 / np.log2(i + 2)
+
+        idgc_20 = dgc_20 / idgc_20
+        all_idgc_20.append(idgc_20)
+
+    if submission:
+        dfs = df[df['rank'] != 100]
+        dfs.sort_values(by=['cell', 'rank'], ascending=True, inplace=True)
+        dfs.drop(labels=['uid', 'y_pred'], axis=1, inplace=True)
+        dfs.to_csv('submission.csv', index=False)
+
+    return np.mean(all_idgc_20)
+
+
+@plac.annotations(
+    cmd=('train, val, rank', 'option', 'c', str)
+)
+def main(cmd: str = 'train'):
     fix_tf()
 
-    if not dummy_data:
-        pass
-    else:
-        xpa = np.round(np.random.uniform(0, 42.5, size=(2048, 400, 1)))
-        xpb = np.random.normal(size=(2048, 400, 2), loc=0.1, scale=4)
-        xp = np.concatenate([xpa, xpb], axis=-1)
-        yp = np.ones((2048, 1))
+    X_train = np.load(os.path.join(TV_PATH, 'X_train.npy'), mmap_mode='r+')
+    y_train = np.load(os.path.join(TV_PATH, 'y_train.npy'))
+    cname_train = np.load(os.path.join(TV_PATH, 'cname_train.npy'))
+    dname_train = np.load(os.path.join(TV_PATH, 'dname_train.npy'))
 
-        xna = np.round(np.random.uniform(0.5, 43, size=(2048, 400, 1)))
-        xnb = np.random.normal(size=(2048, 400, 2), loc=-0.1, scale=4)
-        xn = np.concatenate([xna, xnb], axis=-1)
-        yn = np.zeros((2048, 1))
+    X_val = np.load(os.path.join(TV_PATH, 'X_val.npy'), mmap_mode='r+')
+    y_val = np.load(os.path.join(TV_PATH, 'y_val.npy'))
 
-        X = np.concatenate([xp, xn], axis=0)
-        y = np.concatenate([yp, yn], axis=0)
-
-        indices = np.arange(len(X))
-        np.random.shuffle(indices)
-
-        X = X[indices]
-        y = y[indices]
+    cname_val = np.load(os.path.join(TV_PATH, 'cname_val.npy'))
+    dname_val = np.load(os.path.join(TV_PATH, 'dname_val.npy'))
 
     if cmd == 'train':
         model = build_adaboost()
 
-        model.fit(X, y)
-        y_pred = model.predict(X)
+        model.fit(X_train, y_train)
+        y_pred = np.array(model.predict_proba(X_val))
 
-        print("AUC-ROC: %f" % roc_auc_score(y, y_pred))
+        idgc_20 = idgc20(y_val, y_pred, cname_val, dname_val)
+
+        print("IDGC_20: %f" % idgc_20)
+
+        print("Estimator Weights")
+        print(model.estimator_weights_)
 
         for n, est in enumerate(model.estimators_):
+            print("Saving estimator %d..." % n)
             est.model.save_weights('weights/%s_%d.h5' % (SESSION, n))
 
-    elif cmd == 'val':
+        model.estimators_ = []
+        open('weights/%s.pkl' % SESSION, 'wb').write(pickle.dumps(model))
 
-        models = []
-        preds = np.zeros_like(y)
+    elif cmd == 'val':
+        model = pickle.loads(open('weights/%s.pkl' % SESSION, 'rb').read())
 
         for n in range(0, N_ESTIMATORS):
-            model = build_keras()
-            model.load_weights('weights/%s_%d.h5' % (SESSION, n))
-            models.append(model)
+            m_n = build_sklearn()
+            m_n.model = build_keras()
+            m_n.model.load_weights('weights/%s_%d.h5' % (SESSION, n))
+            model.estimators_.append(m_n)
 
-            preds = preds + model.predict(X)
+        y_pred = np.array(model.predict_proba(X_val))
 
-        y_pred = preds / N_ESTIMATORS
+        idgc_20 = idgc20(y_val, y_pred, cname_val, dname_val)
+        print("IDGC_20: %f" % idgc_20)
 
-        print("AUC-ROC: %f" % roc_auc_score(y, y_pred))
+
+    elif cmd == 'rank':
+        model = pickle.loads(open('weights/%s.pkl' % SESSION, 'rb').read())
+
+        X_test = np.load(os.path.join(TV_PATH, 'X_test.npy'), mmap_mode='r+')
+        cname_test = np.load(os.path.join(TV_PATH, 'cname_test.npy'))
+        dname_test = np.load(os.path.join(TV_PATH, 'dname_test.npy'))
+
+        for n in range(0, N_ESTIMATORS):
+            m_n = build_sklearn()
+            m_n.model = build_keras()
+            m_n.model.load_weights('weights/%s_%d.h5' % (SESSION, n))
+            model.estimators_.append(m_n)
+
+        y_pred = np.array(model.predict_proba(X_test))
+
+        idgc_20 = idgc20(None, y_pred, cname_test, dname_test, submission=True)
 
 
 if __name__ == '__main__':
-    main('val')
+    plac.call(main)
 
--- tvsplit.py
+++ tvsplit.py
@@ -3,111 +3,212 @@ import pandas as pd
 import numpy as np
 import random
 import os
-from sklearn.preprocessing import LabelEncoder
 from rdkit import Chem
 from feature import mol_to_feature
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from cfg import *
+import pickle
 
 
 def npsave(path, arr: np.ndarray):
     print("%s: %s" % (path, arr.shape))
 
-    if not os.path.exists('data'):
-        os.mkdir('data')
+    if not os.path.exists(TV_PATH):
+        os.mkdir(TV_PATH)
 
-    np.save(os.path.join('data', path), arr)
+    np.save(os.path.join(TV_PATH, path), arr)
 
 
 @plac.annotations(
     tvsplit=('Percentage of training data', 'option', 'v', float),
-    seed=('Random seed', 'option', 's', 'int')
+    seed=('Random seed', 'option', 's', int),
+    verbose=('Verbose output', 'option', 'V', int)
 )
 def main(tvsplit: float = 0.8,
-         seed: int = 0):
-
+         seed: int = 0,
+         verbose: int = 0):
     random.seed(seed)
     np.random.seed(seed)
 
-    df_maccs = pd.read_csv('fingerprint.csv')
-    df_train = pd.read_csv('train.csv')
-    df_cell = pd.read_csv('gene_expression.csv')
-    df_profiles = pd.read_csv('profiles.csv')
+    target_nnf = pickle.loads(open(os.path.join(OBJ_PATH, 'target_gene_nnf.pickle'), 'rb').read())
+    target_le = pickle.loads(open(os.path.join(OBJ_PATH, 'target_gene_labelencoder.pickle'), 'rb').read())
+    lines_nnf = pickle.loads(open(os.path.join(OBJ_PATH, 'gene_expression_nnf.pickle'), 'rb').read())
 
-    cells = []
+    def run(stage: str = 'train'):
 
-    labels_total = 0
-    for cell in df_train['cell'].unique():
-        X = []
-        y = []
+        last_lines = None
 
-        for ridx, row in df_train[df_train['cell'] == cell].iterrows():
+        print("Load gene expressions...")
+        df_cell = pickle.loads(open(os.path.join(CSV_PATH, 'cell', 'gene_expression.pkl'), 'rb').read())
+        print("Loading MACCS...")
+        df_maccs = pickle.loads(open(os.path.join(CSV_PATH, 'drug', 'fingerprint.pkl'), 'rb').read())
+        print("Loading profiles")
+        df_profiles = pickle.loads(open(os.path.join(CSV_PATH, 'drug', 'profiles.pkl'), 'rb').read())
+        print("Loading target genes...")
+        df_target = pickle.loads(open(os.path.join(CSV_PATH, 'drug', 'target_gene.pkl'), 'rb').read())
+
+        if stage == 'train':
+            print("Loading training data...")
+            df_train = pd.read_csv(os.path.join(CSV_PATH, 'train.csv'))
+
+            # Attempt to balance classes
+            df_train_pos = df_train[df_train['ri'] == 1]
+            df_train_neg = df_train[df_train['ri'] == 0].sample(df_train_pos.shape[0])
+
+            df_train = pd.concat([df_train_pos, df_train_neg], axis=0)
+        elif stage == 'test':
+            print("Loading test data...")
+            df_train = pd.read_csv(os.path.join(CSV_PATH, 'test.csv'))
+
+        cells = []
+
+        labels_total = 0
+
+        print("Transforming cell lines...")
+        lines_all = df_cell.iloc[:, 1:].values.astype(np.float32)
+        lines_all = lines_nnf.transform(lines_all)
+
+        for cidx, cell in enumerate(df_train['cell'].unique()):
+
             # Get Cell Lines
-            lines = df_cell[df_cell['cname'] == cell].iloc[0][LINES_LABELS].to_list()
-            lines = np.pad(lines, [0, SMILES_LEN - len(lines)])
-            lines = lines[..., np.newaxis]
+            dfs_idx = df_cell.index[df_cell['cname'] == cell].to_list()
+            if len(dfs_idx) != 1:
+                print("Missing Cell: %s" % cell)
+                if stage == 'test':
+                    lines = last_lines
+                else:
+                    continue
+            else:
+                lines = lines_all[dfs_idx[0]]
+                last_lines = lines
 
-            # Get MACCS
-            maccs = [float(x) for x in
-                     df_maccs[df_maccs['dname'] == row['drug']].iloc[0]['fps'][1:-1].replace(" ", "").split(",")]
-            maccs = np.pad(maccs, [0, SMILES_LEN - len(maccs)])
-            maccs = maccs[..., np.newaxis]
+            X = []
+            y = []
+            dname = []
+            cname = []
 
-            # Get SMILES
-            profile = df_profiles[df_cell['cname'] == cell].iloc[0]
-            smiles = profile['smiles']
-            mol = Chem.MolFromSmiles(smiles)
-            smiles = np.reshape(mol_to_feature(mol, -1, 400), (400, 42))
+            print("Cell:\t%s" % cell)
 
-            X.append(np.concatenate([smiles, lines, maccs], axis=1))
-            y.append(row['ri'])
+            for ridx, row in df_train[df_train['cell'] == cell].iterrows():
 
-            labels_total += 1
+                features = [lines]
 
-        cells.append({
-            'X': X,
-            'y': y,
-            'labels': len(y)
-        })
+                if MACCS_ENABLE:
+                    # Get MACCS
+                    dfs = df_maccs[df_maccs['dname'] == row['drug']]
+                    if len(dfs) == 1:
+                        maccs = df_maccs[df_maccs['dname'] == row['drug']].iloc[0]['fps']
+                        maccs = np.array(maccs).astype(np.int32)
+                        features.append(maccs)
+                    else:
+                        if verbose > 0:
+                            print("Missing MACCS for %s" % row['drug'])
+                        continue
 
-    # Shuffle cells
-    random.shuffle(cells)
+                if TARGET_ENABLE:
+                    # Get target genes
+                    dfs = df_target[df_target['dname'] == row['drug']]
+                    if len(dfs) == 1:
+                        target = np.zeros((1, len(target_le.classes_, )))
+                        for tidx, trow in dfs.iterrows():
+                            x = target_le.transform(trow['gene'])
+                            for xi in x:
+                                target[0, xi] = 1
+                        target = target_nnf.transform(target)[0]
+                        features.append(target)
+                    else:
+                        if verbose > 0:
+                            print("Missing target genes")
 
-    train = {'maccs': [], 'y': []}
-    val = {'maccs': [], 'y': []}
+                if SMILES_ENABLE:
+                    # Get SMILES
+                    dfs = df_profiles[df_profiles['dname'] == row['drug']]
+                    if len(dfs) > 0:
+                        profile = dfs.iloc[0]
+                        smiles = profile['smiles']
+                        mol = Chem.MolFromSmiles(smiles)
+                        smiles = np.array(mol_to_feature(mol, -1, 400))
+                        features.append(smiles)
+                    else:
+                        print("Missing SMILES for %s" % row['drug'])
+                        continue
 
-    labels_fed = 0
-    while labels_fed < tvsplit*labels_total:
-        cell = cells.pop()
-        labels_fed += cell['labels']
-        del cell['labels']
-        for key in cell:
-            train[key].extend(cell[key])
+                X.append(np.concatenate(features, axis=0))
 
-    indices = np.arange(len(train['X']))
-    np.random.shuffle(indices)
+                cname.append(str(cell))
+                dname.append(str(row['drug']))
 
-    for key in train:
-        train[key] = np.array(key)
-        train[key] = train[key][indices]
-        npsave('%s_train.npy' % key, train[key])
-        del train[key]
+                if stage == 'train':
+                    y.append(np.array(row['ri'])[..., np.newaxis])
 
-    while len(cells) > 0:
+                labels_total += 1
 
-        cell = cells.pop()
-        labels_fed += cell['labels']
-        del cell['labels']
-        for key in cell:
-            val[key].extend(cell[key])
+            cdict = {
+                'X': X,
+                'labels': len(X),
+                'cname': cname,
+                'dname': dname
+            }
 
-    indices = np.arange(len(val['X']))
-    np.random.shuffle(indices)
+            if stage == 'train':
+                cdict['y'] = y
 
-    for key in val:
-        val[key] = np.array(key)
-        val[key] = val[key][indices]
-        npsave('%s_val.npy' % key, val[key])
-        del val[key]
+            cells.append(cdict)
+
+        if stage == 'train':
+            # Shuffle cells
+            random.shuffle(cells)
+
+            train = {'X': [], 'y': [], 'cname': [], 'dname': []}
+            val = {'X': [], 'y': [], 'cname': [], 'dname': []}
+
+            labels_fed = 0
+            while labels_fed < tvsplit * labels_total:
+
+                print("%d < %f : %d left" % (labels_fed, tvsplit * labels_total, len(cells)))
+                cell = cells.pop()
+                labels_fed += cell['labels']
+                del cell['labels']
+                for key in cell:
+                    train[key].extend(cell[key])
+
+            indices = np.arange(len(train['X']))
+            np.random.shuffle(indices)
+
+            for key in train:
+                train[key] = np.array(train[key])
+                train[key] = train[key][indices]
+                npsave('%s_train.npy' % key, train[key])
+                train[key] = None
+
+            while len(cells) > 0:
+                cell = cells.pop()
+                labels_fed += cell['labels']
+                del cell['labels']
+                for key in cell:
+                    val[key].extend(cell[key])
+
+            for key in val:
+                val[key] = np.array(val[key])
+                npsave('%s_val.npy' % key, val[key])
+                val[key] = None
+
+        elif stage == 'test':
+            test = {'X': [], 'cname': [], 'dname': []}
+
+            while len(cells) > 0:
+                cell = cells.pop()
+                del cell['labels']
+                for key in cell:
+                    test[key].extend(cell[key])
+
+            for key in test:
+                test[key] = np.array(test[key])
+                npsave('%s_test.npy' % key, test[key])
+                test[key] = None
+
+    # run(stage='train')
+    run(stage='test')
 
 
 if __name__ == '__main__':
